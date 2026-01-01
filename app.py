d3400066856eda836d2d599a090493600ecd3d83
Hari Kothapalli <harikothapalli314@gmail.com>
2026-01-01 17:53:12 +0530

    Fix SEO routes with absolute paths and remove redundant robots.txt

--- app.py
+++ app.py
@@ -789,15 +789,15 @@ def service_worker():
 
 @app.route('/google10c68f1d7dfe2f5f.html')
 def google_verification():
-    return send_file('google10c68f1d7dfe2f5f.html')
+    return send_file(os.path.join(app.root_path, 'google10c68f1d7dfe2f5f.html'))
 
 @app.route('/sitemap.xml')
 def sitemap():
-    return send_file('sitemap.xml')
+    return send_file(os.path.join(app.root_path, 'sitemap.xml'))
 
 @app.route('/robots.txt')
 def robots():
-    return send_file('robots.txt')
+    return send_file(os.path.join(app.root_path, 'robots.txt'))
 
 @app.route("/compiler")
 @login_required
